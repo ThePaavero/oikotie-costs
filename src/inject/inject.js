dd6ded4a84a36b15b29d33d9ff948c53577ad7de
Pekka <nospam@astudios.org>
2018-03-04 20:58:47 +0200

    Done

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,18 +3,27 @@ const Oikotie = function() {
   const init = () => {
     const matchTitles = ['Rahoitusvastike', 'Hoitovastike']
     const titleSiblings = document.querySelectorAll('.info-table__title')
-    let extraCosts = 0
+    let costs = 0
     Array.from(titleSiblings).forEach(titleElement => {
       const index = matchTitles.indexOf(titleElement.innerText)
       if (index > -1) {
         const valueString = titleElement.parentNode.querySelector('.info-table__value').innerText.split('€')[0].replace(',', '.')
-        extraCosts += parseFloat(valueString)
+        costs += parseFloat(valueString)
       }
     })
 
+    const calculatorResultElement = document.querySelector('.listing-sidepanel-calculator h2')
+    if (calculatorResultElement) {
+      costs += parseFloat(calculatorResultElement.innerText.split('€')[0])
+    }
+
     const displayElement = document.createElement('div')
     displayElement.className = 'oikotie-computed-extra-costs-wrapper'
-    displayElement.innerText = '+ ' + extraCosts + ' € / kk'
+    displayElement.innerText = costs + ' € / kk'
+    displayElement.addEventListener('click', e => {
+      displayElement.remove()
+      init()
+    })
     document.body.appendChild(displayElement)
   }
 
